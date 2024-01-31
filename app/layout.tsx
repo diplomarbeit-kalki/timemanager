c31276331dcce88adf7e9026224f55a2f47cd53d
Tobias Mitterwallner <tobias.mitterwallner@htl-wels.at>
2024-01-31 09:33:10 +0100

    Changed name of providers for clarification

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./ui/providers";
+import { Providers } from "./ui/ThemeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
