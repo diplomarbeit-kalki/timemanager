20002ac99d11f3776daf71ae76d008ec50725e53
Tobias Mitterwallner <tobias.mitterwallner@htl-wels.at>
2024-01-31 09:25:34 +0100

    First Attempt on Landing Page, Added darkMode (not every site adapted yet)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,14 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
     },
   },
   plugins: [],
+  darkMode: 'class',
 };
 export default config;
