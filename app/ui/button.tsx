a8c401c6d72f1320ff8565d16f67c1832659175e
Tobias Mitterwallner <tobias.mitterwallner@htl-wels.at>
2024-01-31 12:22:13 +0100

    Implemented new Login Form optimized for dark and light mode

--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -4,12 +4,12 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export function Button({ children, className, ...rest }: ButtonProps) {
+export function   Button({ children, className, ...rest }: ButtonProps) {
   return (
     <button
       {...rest}
       className={clsx(
-        'flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
+        'flex h-10 items-center rounded-lg bg-blue-500 hover:bg-blue-600 dark:bg-slate-700 dark:hover:bg-slate-800 text-white dark:text-black px-4 text-sm font-medium transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 dark:focus-visible:outline-white active:bg-blue-600 dark:active:bg-gray-200 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
         className,
       )}
     >
