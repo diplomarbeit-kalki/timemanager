fa201b859b7f2b7d3233b522201fc6e442d8593a
eliaskalkhofer <elias.kalkhofer@htl-wels.at>
2024-01-31 12:18:43 +0100

    Implemented show employee functionallity

--- app/dashboard/employeelist/[id]/show/page.tsx
+++ app/dashboard/employeelist/[id]/show/page.tsx
@@ -1,12 +1,45 @@
-import { lusitana } from '@/app/ui/fonts';
+import Form from '@/app/ui/employeelist/show-form';
+import Breadcrumbs from '@/app/ui/employeelist/breadcrumbs';
+import { fetchEmployeeById } from '@/app/lib/data/datafetching';
+import { notFound } from 'next/navigation';
+import { ObjectId } from 'mongodb';
 
-export default async function Page() {
+export default async function Page({ params }: { params: { id: string } }) {
+
+    const id = params.id;
+    const oid = new ObjectId(id);
+    const employee = await fetchEmployeeById(oid);
+
+    if (!employee) {
+        notFound();
+    }
+
+    const employeeForm = {
+        id: id,
+        firstname: employee.firstname,
+        lastname: employee.lastname,
+        birthdate: employee.birthdate,
+        street: employee.street,
+        housenr: employee.housenr,
+        residence: employee.residence,
+        postalcode: employee.postalcode,
+        phonenr: employee.phonenr,
+        email: employee.email,
+    }
 
     return (
-        <div className="w-full">
-            <div className="flex w-full items-center justify-between">
-                <h1 className={`${lusitana.className} text-2xl`}>Show</h1>
-            </div>
-        </div>
-    )
+        <main>
+            <Breadcrumbs
+                breadcrumbs={[
+                    { label: 'Mitarbeiter', href: '/dashboard/employeelist' },
+                    {
+                        label: 'Ansehen',
+                        href: `/dashboard/employeelist/${id}/show`,
+                        active: true,
+                    },
+                ]}
+            />
+            <Form employee={employeeForm} />
+        </main>
+    );
 }
