2713b2cabb4e01f1b005c9e6c503b53000e72985
Tobias Mitterwallner <tobias.mitterwallner@htl-wels.at>
2024-01-31 20:33:15 +0100

    Updated colors for buttons and employeelist (darkmode)

--- app/ui/employeelist/buttons.tsx
+++ app/ui/employeelist/buttons.tsx
@@ -3,25 +3,27 @@ import { EyeIcon, PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/ou
 import Link from 'next/link';
 import { deleteEmployee } from '@/app/lib/actions/dbactions';
 
-export function ShowEmployee({ id }: { id: string }) {
-    return (
-        <Link
-            href={`/dashboard/employeelist/${id}/show`}
-            className="rounded-md border p-2 hover:bg-gray-100"
-        >
-            <EyeIcon className="w-5" />
-        </Link>
-    );
-}
-
 export function CreateEmployee() {
     return (
         <Link
             href="/dashboard/employeelist/create"
-            className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+            className="flex h-10 items-center rounded-lg bg-blue-600 dark:bg-cyan-900 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
         >
             <span className="hidden md:block">Mitarbeiter hinzufügen</span>{' '}
             <PlusIcon className="h-5 md:ml-4" />
+        </Link>
+    );
+}
+
+//TODO: Andere Buttons auch anpassen wie ShowEmployee
+
+export function ShowEmployee({ id }: { id: string }) {
+    return (
+        <Link
+            href={`/dashboard/employeelist/${id}/show`}
+            className="rounded-md border border-gray-400 dark:border-gray-500 p-2 bg-gray-300 dark:bg-gray-600 hover:bg-gray-400 dark:hover:bg-gray-700"
+        >
+            <EyeIcon className="w-5" />
         </Link>
     );
 }
@@ -30,7 +32,7 @@ export function UpdateEmployee({ id }: { id: string }) {
     return (
         <Link
             href={`/dashboard/employeelist/${id}/edit`}
-            className="rounded-md border p-2 hover:bg-gray-100"
+            className="rounded-md border border-gray-400 dark:border-gray-500 p-2 bg-orange-300 dark:bg-orange-400 hover:bg-orange-400 dark:hover:bg-orange-500"
         >
             <PencilIcon className="w-5" />
         </Link>
@@ -42,9 +44,9 @@ export function DeleteEmployee({ id }: { id: string }) {
 
     return (
         <form action={deleteEmployeeWithId}>
-            <button className="rounded-md border p-2 hover:bg-gray-100">
+            <button className="rounded-md border border-gray-400 dark:border-gray-500 p-2 bg-red-300 dark:bg-red-500 hover:bg-red-400 dark:hover:bg-red-600">
                 <span className="sr-only">Delete</span>
-                <TrashIcon className="w-4" />
+                <TrashIcon className="w-5" />
             </button>
         </form>
     );
--- app/ui/employeelist/table.tsx
+++ app/ui/employeelist/table.tsx
@@ -28,13 +28,13 @@ export default async function HospitationTable({
     return (
         <div className="mt-6 flow-root">
             <div className="inline-block min-w-full align-middle">
-                <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
+                <div className="rounded-lg bg-gray-50 dark:bg-zinc-950 p-2 md:pt-0">
                     <div className="md:hidden">
 
                         {employees?.map((employee) => (
                             <div
                                 key={String(employee._id)}
-                                className="mb-2 w-full rounded-md bg-white p-4"
+                                className="mb-2 w-full rounded-md bg-white dark:bg-zinc-950 p-4"
                             >
                                 <div className="flex w-full items-center justify-between pt-4">
                                     <div>
@@ -63,7 +63,7 @@ export default async function HospitationTable({
 
                     </div>
                     <table className="hidden min-w-full text-gray-900 md:table">
-                        <thead className="rounded-lg text-left text-sm font-normal">
+                        <thead className="rounded-lg text-left text-sm font-bold dark:text-gray-400">
                             <tr>
                                 <th scope="col" className="px-5 py-5 font-medium sm:pl-6">
                                     Psnr
@@ -86,11 +86,11 @@ export default async function HospitationTable({
                             </tr>
                         </thead>
 
-                        <tbody className="bg-white">
+                        <tbody className="bg-white dark:bg-zinc-800 dark:text-white">
                             {employees?.map((employee) => (
                                 <tr
                                     key={String(employee._id)}
-                                    className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                                    className="w-full border-b dark:border-gray-600 py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
                                 >
                                     <td className="whitespace-nowrap px-5 py-3">{employee.psnr}</td>
                                     <td className="whitespace-nowrap px-5 py-3">{employee.username}</td>
