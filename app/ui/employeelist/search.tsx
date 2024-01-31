70d0f4e9105e817eb071d74c3cf7c17b458a91e4
eliaskalkhofer <elias.kalkhofer@htl-wels.at>
2024-01-31 09:45:10 +0100

    Added search and pagination for Mitarbeiter/employeelist

--- app/ui/employeelist/search.tsx
+++ app/ui/employeelist/search.tsx
@@ -20,7 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         params.delete('query');
       }
       replace(`${pathname}?${params.toString()}`);
-    }, 300);
+    }, 100);
 
     return (
       <div className="relative flex flex-1 flex-shrink-0">
