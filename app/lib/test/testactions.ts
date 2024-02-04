62d2db13c1e1d1214f96c631eef85e3842e70dc0
eliaskalkhofer <elias.kalkhofer@htl-wels.at>
2024-02-04 13:21:23 +0100

    Datafetching REST vs Server Actions (lib/test/testactions)

--- app/lib/test/testactions.ts
+++ app/lib/test/testactions.ts
@@ -1,7 +1,37 @@
 "use server"
 import { ObjectId } from 'mongodb';
 import DatabaseClient from '@/app/lib/mongodb/client';
-import { getUser } from '@/app/lib/auth/authuser';
+import axios from 'axios';
+import  { fetchEmployeeById } from '@/app/lib/data/datafetching';
+
+const idstring = "65bb6eca319adc7a6e45df6d";
+const id = new ObjectId("65bb6eca319adc7a6e45df6d");
+
+//Funktioniert nur wenn man timerest laufen hat.
+async function fetchData(idstring: string): Promise<void> {
+    console.time("REST API Fetch Time");
+    const apiUrl = `http://localhost:3001/employees/byId/${idstring}`;
+    try {
+        const response = await axios.get(apiUrl);
+        console.log("REST---Daten erhalten!");
+    } catch (error) {
+        console.error('REST---Fehler beim Abrufen der Daten:', error);
+    } finally {
+        console.timeEnd("REST API Fetch Time");
+    }
+}
+
+async function fetchDataWithServerAction(id: ObjectId) {
+    console.time("Server Action Fetch Time");
+    try {
+        const result = await fetchEmployeeById(id);
+        console.log("Server action---Daten erhalten!");
+    } catch (error) {
+        console.error('Server action---Fehler beim Abrufen der Daten:', error);
+    } finally {
+        console.timeEnd("Server Action Fetch Time"); 
+    }
+}
 
 export async function testfunction() {
 
@@ -10,14 +40,12 @@ export async function testfunction() {
     console.log("Testaction called by Button");
     try {
         console.log("Try ...");
-        const user = await getUser("KALELI");
-        console.log("testactions---User: " + JSON.stringify(user));
+        console.log("Start Datfetching REST!");
+        await fetchData(idstring);
+        console.log("Start Datfetching Server Action!");
+        await fetchDataWithServerAction(id);
     }
     catch (error) {
         console.error('testactions---Fehler: ', error);
-    }
-    finally {
-     
-        console.log("testactions---Datenbankverbindung geschlossen");
     }
 }
