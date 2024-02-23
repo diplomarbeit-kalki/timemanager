875afed444f8dd3be4b9495332f3cd3adb3e256a
eliaskalkhofer <elias.kalkhofer@htl-wels.at>
2024-02-23 19:36:07 +0100

    Updated placeholder-data/seed script, updated images functionallity on timeclock page

--- app/dashboard/test/test2/page.tsx
+++ app/dashboard/test/test2/page.tsx
@@ -1,6 +1,5 @@
-"use client"
 import { lusitana } from '@/app/ui/fonts';
-import React, { useEffect, useState } from 'react';
+import Image from "next/image"
 
 export default function Page() {
 
@@ -8,7 +7,14 @@ export default function Page() {
         <div className="w-full">
             <div className="flex w-full items-center justify-between">
                 <h1 className={`${lusitana.className} text-2xl`}>Testpage 2</h1>
-            </div>   
+            </div>
+            <Image
+                src='/profilepictures/placeholder.jpg'
+                alt="Profilbild"
+                width={500}
+                height={500}
+                className="rounded-2xl"
+            />
         </div>
     );
 }
